683ad9b71b18c60264552f472b215bfed68a715a
socratone <gim2origin@gmail.com>
2021-05-01 22:50:34 +0900

    주석 추가

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -3,15 +3,15 @@ const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'
 
 module.exports = {
   mode: 'development',
-  devtool: 'cheap-module-source-map',
+  devtool: 'cheap-module-source-map', // create react app에서 추천하는 dev용 source map
   devServer: {
     hot: true,
     open: true,
   },
   plugins: [
-    new ReactRefreshWebpackPlugin(),
+    new ReactRefreshWebpackPlugin(), // hot load
     new webpack.DefinePlugin({
-      'process.env.name': JSON.stringify('Vishwas'),
+      'process.env.name': JSON.stringify('Vishwas'), // 환경 변수 설정
     }),
   ],
 }
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -5,7 +5,7 @@ module.exports = {
   devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.name': JSON.stringify('Codevolution'),
+      'process.env.name': JSON.stringify('Codevolution'), // 환경 변수 설정
     }),
   ],
 }
